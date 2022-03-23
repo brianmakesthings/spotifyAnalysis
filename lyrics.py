840717d4473d3dbd13e6bed7864999ff08018992
milak <milak@sfu.ca>
2022-03-22 22:12:42 -0700

    added tfidf

--- lyrics.py
+++ lyrics.py
@@ -6,8 +6,9 @@ import re
 import ast
 from nltk.corpus import stopwords
 from swaglyrics.cli import get_lyrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
-pd.options.mode.chained_assignment = None
 
 
 
@@ -21,7 +22,7 @@ def filter_lyrics(playlist_row, cachedStopWords):
     lyrics = playlist_row['lyrics'][0]
     
     if lyrics is '' or lyrics is None:
-        return 'Error: No lyrics found'
+        return ['Error: No lyrics found']
    
     # replace characters/words that are not a part of the lyrics with a space
     lyrics = remove(lyrics)
@@ -29,33 +30,24 @@ def filter_lyrics(playlist_row, cachedStopWords):
     # remove stopwords
     lyrics = ' '.join([word for word in lyrics.lower().split() if word not in cachedStopWords])
     
-    return lyrics
+    # frequency of each word
+    count_transformer = CountVectorizer()
+    lyrics_count = count_transformer.fit_transform([lyrics])
     
+    tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+    tfidf_transformer.fit(lyrics_count)
+    tf_idf = tfidf_transformer.transform(lyrics_count)
+    words = count_transformer.get_feature_names()
     
+    tfidf_df = pd.DataFrame(tf_idf[0].T.todense(), index=count_transformer.get_feature_names(), columns=["tf-idf"])
     
+    tfidf_df = tfidf_df[tfidf_df["tf-idf"]>0.1]
+    tfidf_df = tfidf_df.sort_values(by=["tf-idf"], ascending=False) 
+    return tfidf_df.to_json(orient = 'index')
+
     
-def tf_idf(playlists):
+
     
-    lyrics = list(playlists["lyrics_filtered"])
-    
-    tfidf = TfidfVectorizer(use_idf = True, lowercase = False, stop_words = {'english'})
-    lyrics_tfidf = tfidf.fit_transform(lyrics)
-    index = tfidf.get_feature_names()
-    
-    playlists['lyrics_tfidf'] = playlists['lyrics_filtered']
-    count = 0
-    
-    for item in lyrics_tfidf:
-        if playlists['lyrics_tfidf'][count] is not 'Error: No lyrics found':
-            results = pd.DataFrame(lyrics_tfidf[count].T.todense(), index=index, columns=["tfidf"])
-            results = results.sort_values(by=["tfidf"],ascending=False)
-            results = results[~results.index.duplicated()]
-            results = results[results["tfidf"]>0.2]
-            playlists['lyrics_tfidf'][count] = results.to_json(orient = 'index')
-            
-        count = count + 1
-    
-    return playlists
     
 
 
@@ -77,12 +69,11 @@ def main(playlist_csv):
     
     print("Filtering lyrics ...")
     
-    nltk.download('stopwords')
+#     nltk.download('stopwords')
     cachedStopWords = stopwords.words("english")
 #     songs_df['lyrics'] = songs_df['lyrics'].apply(ast.literal_eval)
-    songs_df['lyrics_filtered'] = songs_df.apply(lambda row : filter_lyrics(row, cachedStopWords), axis=1)
-    songs_df = tf_idf(songs_df)
-    songs_df.to_csv("playlist_lyrics.csv")
+    songs_df['lyrics_tfidf'] = songs_df.apply(lambda row : filter_lyrics(row, cachedStopWords), axis=1)
+    songs_df.to_csv("playlist_lyrics_test.csv")
     
     print("Done")
 
